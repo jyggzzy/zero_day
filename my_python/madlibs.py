948b45ddfb1cfad36e99aa72e7c02cb122f5fcf5
jyggzzy <jyggzzy@yahoo.com>
2022-08-22 07:32:17 -0700

    just_python

---
+++ my_python/madlibs.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+sport = input("What's your favorite sport? ")
+noun = input("Enter a noun: ")
+preposition = input("Enter a preposition: ")
+
+print("My name is " + name)
+print("I'm a lover of " + sport)
+print(noun + " is my favorite game")
+print("There is no reason I cannot rule the world " + preposition " you")
