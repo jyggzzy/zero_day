bb0c74c729115be0d47092f6b19d97dc2b77172c
jyggzzy <jyggzzy@yahoo.com>
2022-09-10 12:17:14 -0700

    origin

---
+++ guess.py
@@ -0,0 +1,17 @@
+import random
+
+
+def guess(x):
+    random_number = random.randint(5, x)
+    guess = 
+    while guess != random_number:
+        guess = int(input(f"Guess Man Utd's position at end of season 💀: "))
+        if guess > random_number:
+            print("Give yourself some credit; you're not that poor 😊")
+        elif guess < random_number:
+            print("haq haq haq haq haq. Be realistic bro 🌚")
+
+    print(f"You even sabi say na {random_number}'th be the best wey you go fit carry las las 😂😋💀")
+
+
+guess(10)
